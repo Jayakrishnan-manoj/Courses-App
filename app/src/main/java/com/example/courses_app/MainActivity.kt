f9c1c41166ef4629a940fe789b1b5c90885a57bd
jk.jayakrishnan27 <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2024-02-01 23:58:17 +0530

    create: hommescreen layout and appbar

--- app/src/main/java/com/example/courses_app/MainActivity.kt
+++ app/src/main/java/com/example/courses_app/MainActivity.kt
@@ -1,15 +1,26 @@
- package com.example.courses_app
+package com.example.courses_app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.courses_app.ui.theme.Courses_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,17 +41,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Courses_AppTheme {
-        Greeting("Android")
+fun HomeScreeen(
+    onDetailsClick: (title: String) -> Unit,
+    onAboutClick: () -> Unit
+) {
+    Scaffold { padding ->
+        LazyColumn(contentPadding = padding) {
+            item {
+                HomeAppBar(onAboutClick)
+            }
+            item {
+                Spacer(modifier = Modifier.height(30.dp))
+            }
+            items(allCourses) { item ->
+                ItemCard(item, onDetailsClick(item.title))
+            }
+        }
     }
 }
+
+@Composable
+private fun HomeAppBar(onAboutClick: () -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 10.dp)
+    ) {
+        Text(text = "My Udemy Courses", style = MaterialTheme.typography.headlineLarge)
+        Spacer(modifier = Modifier.weight(1f))
+        TextButton(onClick = onAboutClick) {
+            Text(text = "About", fontSize = 24.sp)
+
+        }
+    }
+}
+
