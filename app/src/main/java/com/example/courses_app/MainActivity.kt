1a2cc37127cdb76e5501ef3d154af2edb9d37d9d
jk.jayakrishnan27 <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2024-02-02 00:43:30 +0530

    create:Course Card

--- app/src/main/java/com/example/courses_app/MainActivity.kt
+++ app/src/main/java/com/example/courses_app/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.courses_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -18,6 +24,8 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -54,7 +62,7 @@ fun HomeScreeen(
                 Spacer(modifier = Modifier.height(30.dp))
             }
             items(allCourses) { item ->
-                ItemCard(item, onDetailsClick(item.title))
+                CourseCard(item, onDetailsClick(item.title))
             }
         }
     }
@@ -71,6 +79,37 @@ private fun HomeAppBar(onAboutClick: () -> Unit) {
         TextButton(onClick = onAboutClick) {
             Text(text = "About", fontSize = 24.sp)
 
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CourseCard(item: Courses, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(vertical = 10.dp, horizontal = 16.dp)
+            .fillMaxWidth(),
+        onClick = onClick
+    ) {
+        Column {
+            Image(
+                painterResource(id = item.thumbnail),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .aspectRatio(16f / 9f),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+            ) {
+                Text(text = item.title)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(item.body, maxLines = 1, style = MaterialTheme.typography.bodySmall)
+            }
         }
     }
 }
